ccf1d9f0d5b47fe771a663eddd038ea7c572abca
fankai <006512@crfsdi.com>
2024-08-09 19:08:40 +0800

    增加列表表头筛选组件--日期、日期时间

--- platform.js
+++ platform.js
@@ -89,3 +89,27 @@ var PLATFORM_UTILS = {
         GLOBAL_DATA.hasOpenMenusId.delete($(_curClosePanel).panel("options").id)
     }
 }
+
+
+
+var _easyuiDateboxClearButtons = $.extend([], $.fn.datebox.defaults.buttons);
+_easyuiDateboxClearButtons.splice(2, 0, {
+    text: '清除',
+    handler: function (target) {
+        $(target).datebox("clear");
+        $(target).datebox("hidePanel");
+    }
+});
+var _easyuiDatetimeboxClearButtons = $.extend([], $.fn.datetimebox.defaults.buttons);
+_easyuiDatetimeboxClearButtons.splice(3, 0, {
+    text: '清除',
+    handler: function (target) {
+        $(target).datebox("clear");
+        $(target).datebox("hidePanel");
+    }
+});
+
+var EASYUI_UTILS = {
+    dateboxClearButtons:_easyuiDateboxClearButtons,
+    datetimeboxClearButtons:_easyuiDatetimeboxClearButtons
+}
